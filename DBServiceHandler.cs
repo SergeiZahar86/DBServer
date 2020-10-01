bf7c67e8bbe5607b83f983bff483597004597c7a
Сергей Захаров <sergei86zahar@mail.ru>
2020-10-01 16:47:32 +0700

    Добавьте файлы проекта.

---
+++ DBServiceHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBServer
+{
+    class DBServiceHandler : DBService.Iface
+    {
+        bool DBService.ISync.addRow(Row row)
+        {
+            return true;
+        }
+
+        bool DBService.ISync.delRow(int ID)
+        {
+            return true;
+        }
+
+        bool DBService.ISync.editRow(Row row)
+        {
+            return true;
+        }
+
+        List<Row> DBService.ISync.listRow()
+        {
+            List<Row> list = new List<Row>();
+            Row row = new Row();
+            row.ID = 1;
+            row.Login = "sergei";
+            row.Password = "zahar";
+            list.Add(row);
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace DBServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBServiceHandler service = new DBServiceHandler();
+            DBService.Processor processor = new DBService.Processor(service);
+            TServerTransport transport = new TServerSocket(9090, 1000);
+            TServer server = new TSimpleServer(processor, transport);
+
+            server.Serve();
+        }
+    }
+}
